3cacdedc245100d8d8ec6a61edfe5014318b195e
Zeb Kleinsorge <zeb.kleinsorge@gmail.com>
2024-11-26 18:34:01 -0500

    11-25 meeting updates

--- slideshow.js
+++ slideshow.js
@@ -1,21 +1,24 @@
 let slideIndex = 0;
 let slideInterval;
+let numSlides;
 
 function showSlides() {
     let slides = document.getElementsByClassName("slide");
+    numSlides = slides.length;
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;    
+    }
+    // console.log("Displaying slide at index:", slideIndex - 1);
     slides[slideIndex - 1].style.display = "block";  
 }
 
 // Function to start the slideshow with a set interval
 function startSlideshow() {
-    showSlides();
     slideInterval = setInterval(() => {
-        slideIndex++;
         showSlides();
     }, 5000); // Adjust the time interval between slides as needed
 }
@@ -24,9 +27,13 @@ function startSlideshow() {
 function plusSlides(n) {
     clearInterval(slideInterval); // Stop any previous interval
     slideIndex += n - 1;
+    if (slideIndex == -1) {
+        slideIndex = numSlides - 1
+    }
     showSlides();
     startSlideshow(); // Restart the interval timer
 }
 
 // Start the slideshow when the page loads
+showSlides();
 startSlideshow();
